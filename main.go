289104b17256e2c30be8488eb43cab84136e1528
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-06-30 22:09:37 +0100

    update: get ip address

--- main.go
+++ main.go
@@ -4,17 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
 )
-
-func jsonResponse(w http.ResponseWriter, v map[string]string) {
-	err := json.NewEncoder(w).Encode(v)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
-}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -35,7 +29,7 @@ func main() {
 		}
 
 		jsonResponse(w, map[string]string{
-			"client_ip": strings.Split(r.RemoteAddr, ":")[0],
+			"client_ip": getIPAddressFromRequest(r),
 			"location":  "New York",
 			"greeting":  fmt.Sprintf("Hello, %s!, the temperature is 11 degrees Celsius in New York", name),
 		})
@@ -44,5 +38,36 @@ func main() {
 	log.Println("listening on", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal("could not start server:", err)
+	}
+}
+
+func getIPAddressFromRequest(r *http.Request) string {
+	ips := r.Header.Get("X-Forwarded-For")
+	splitIps := strings.Split(ips, ",")
+
+	if len(splitIps) > 0 {
+		netIP := net.ParseIP(splitIps[len(splitIps)-1])
+		if netIP != nil {
+			return ""
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return ""
+	}
+
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		return netIP.String()
+	}
+
+	return ""
+}
+
+func jsonResponse(w http.ResponseWriter, v map[string]string) {
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
 }
